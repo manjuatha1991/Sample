dfffa067ac70e6797db1e75d965f89442a4f0ad2
manjuatha1991 <59548756+manjuatha1991@users.noreply.github.com>
2020-01-07 08:29:05 +0530

    Update Login.java

--- src/login/Login.java
+++ src/login/Login.java
@@ -5,7 +5,7 @@ public class Login {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("WElcome");
+		System.out.println("Manjunatha");
 	}
 
 }
